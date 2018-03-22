d397e764ae4a9524505cdc37441ba71b212b935f
Dominik Vesely <dominik.vesely@ackee.cz>
2018-03-22 18:59:44 +0100

    Initial commit

---
+++ CodeLikeUs/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CodeLikeUs
+
+//
+//  Created by Petr Šíma on 01/02/2018.
+//  Copyright © 2018 Petr Šíma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+use_frameworks!
+
+platform :ios, '9.0'
+inhibit_all_warnings!
+
+target 'CodeLikeUs' do
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+end
